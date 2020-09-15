f12196606409ab13d589ea34a1c1da7e73c77c2a
paulobef <beffpa@gmail.com>
2020-09-16 00:15:03 +0200

    load dotenv file for production

--- config.py
+++ config.py
@@ -1,25 +1,27 @@
 """App configuration."""
-from os import environ
-import redis
+import os
+from dotenv import load_dotenv
+
+load_dotenv('.env')
 
 
 class Config:
     """Set Flask configuration vars from .env file."""
 
     # General Config
-    SECRET_KEY = environ.get('SECRET_KEY')
-    FLASK_APP = environ.get('FLASK_APP')
-    FLASK_ENV = environ.get('FLASK_ENV')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_ENV = os.environ.get('FLASK_ENV')
 
     # Flask-Redis
-    REDIS_URL = environ.get('REDIS_URL')
+    REDIS_URL = os.environ.get('REDIS_URL')
 
-    ACCESS_TOKEN = environ.get('ACCESS_TOKEN')
-    VERIFY_TOKEN = environ.get('VERIFY_TOKEN')
+    ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
+    VERIFY_TOKEN = os.environ.get('VERIFY_TOKEN')
 
     # Dropbox
-    DROPBOX_ACCESS_TOKEN = environ.get('DROPBOX_ACCESS_TOKEN')
+    DROPBOX_ACCESS_TOKEN = os.environ.get('DROPBOX_ACCESS_TOKEN')
 
-    SUBSCRIPTION_KEY = environ.get('SUBSCRIPTION_KEY')
-    ENDPOINT = environ.get('ENDPOINT')
+    SUBSCRIPTION_KEY = os.environ.get('SUBSCRIPTION_KEY')
+    ENDPOINT = os.environ.get('ENDPOINT')
 
